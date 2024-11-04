e575de6ab5330081614aa5ccfef5351d777ed42a
YuyoAndrade <euyuyo01@gmail.com>
2024-11-03 22:56:25 -0600

    Add models module and start LSTM

--- database/utils.py
+++ database/utils.py
@@ -1,5 +1,6 @@
-from connection import get_connection
+from .connection import get_connection
 from sqlalchemy import text
+import pandas as pd
 
 
 def get_table(table):
@@ -9,5 +10,21 @@ def get_table(table):
 
 def get_specifics(columns, table):
     db = get_connection()
-    string = ", ".join(columns)
-    return db.execute(text(f"select {string} from {table}"))
+    columns = [f'{table}."{c}"' for c in columns]
+    statement = ", ".join(columns)
+    return db.execute(text(f"select {statement} from {table} where moneda_cve = 2"))
+
+
+def create_dataframe(columns, table):
+    result = get_specifics(columns=columns, table=table)
+
+    df = pd.DataFrame(result.fetchall(), columns=result.keys())
+    df = df.drop_duplicates()
+
+    df["Fecha_hoy"] = pd.to_datetime(df["Fecha_hoy"])
+    df["weekly"] = df["Fecha_hoy"].dt.strftime("%Y-%W")
+
+    df = df.drop("Fecha_hoy", axis=1)
+    df = df.groupby(by="weekly").sum()
+
+    return df.sort_values(by="weekly", axis=0, ascending=True)
--- models/neuralnetworks.py
+++ models/neuralnetworks.py
@@ -1,41 +1,90 @@
-# import tensorflow as tf
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import InputLayer, LSTM, Dropout, Dense
+from tensorflow.keras.layers import InputLayer, Dropout, Dense
+from tensorflow.keras.layers import LSTM as layerLSTM
+
+from .model import Model
+from .utils import get_train_validation_data, get_test_data, create_sequences
 
 
-class NeuralNetwork:
-    def __init__(self, model_type, name, created_at, version, metrics=None):
-        self.model_type = model_type
-        self.name = name
-        self.created_at = created_at
-        self.version = version
-        self.metrics = metrics
+class LSTM(Model):
+    def __init__(
+        self,
+        name,
+        created_at,
+        version,
+        length=10,
+        metrics=None,
+        model_type=None,
+    ):
+        self.model_type = "LSTM"
+        self.length = length
 
-    def metrics(self):
-        pass
-
-    def predict(self):
-        pass
-
-    def LSTM(self, length):
+    def build_model(self):
         model = Sequential()
-        model.add(InputLayer(input_shape=(length, 1)))
+        model.add(InputLayer(shape=(self.length, 1)))
         # First LSTM layer
-        model.add(LSTM(units=50, return_sequences=True))
+        model.add(layerLSTM(units=150, return_sequences=True))
         model.add(Dropout(0.2))
 
         # Second LSTM layer
-        model.add(LSTM(units=50, return_sequences=False))
+        model.add(layerLSTM(units=150))
         model.add(Dropout(0.2))
 
         # Dense layer
-        model.add(Dense(units=25, activation="relu"))
+        model.add(Dense(units=75, activation="relu"))
+        model.add(Dropout(0.2))
+
+        # Dense layer
+        model.add(Dense(units=75, activation="relu"))
         model.add(Dropout(0.2))
 
         # Output layer
-        model.add(Dense(units=1))  # Linear activation is default for regression
+        model.add(Dense(units=1))
 
-        # metrics = ""
-        # self.metrics = self.
-        # return self
-        return self
+        return model
+
+    def training(self, dataset):
+        dataset = dataset.to_numpy()
+        model = self.build_model()
+        x, y = create_sequences(dataset, self.length)
+        model.compile(
+            loss="mean_squared_error",
+            optimizer=Adam(learning_rate=0.01),
+            metrics=["mean_absolute_error"],
+        )
+        model.summary()
+
+        num_train, num_validation = get_train_validation_data(data=dataset, train=0.8)
+
+        x_train = x[:][:num_train]
+        x_validation, y_validation = (
+            x[:][num_train : num_train + num_validation],
+            y[:][num_train : num_train + num_validation],
+        )
+        y_train = y[:][:num_train]
+
+        x_train = x_train.reshape((x_train.shape[0], 10, 1))
+        x_validation = x_validation.reshape((x_validation.shape[0], 10, 1))
+
+        callbacks = [EarlyStopping(patience=10, restore_best_weights=True)]
+
+        model.fit(
+            x_train,
+            y_train,
+            validation_data=(x_validation, y_validation),
+            epochs=100,
+            batch_size=64,
+            callbacks=callbacks,
+            shuffle=False,
+        )
+        return model
+
+    def test(self, model, dataset, test):
+        num_test = get_test_data(data=dataset.to_numpy(), test=test)
+        x, y = create_sequences(dataset.to_numpy(), self.length)
+        x_test = x[:][-num_test:]
+        y_test = y[:][-num_test:]
+
+        return model.evaluate(x_test, y_test)
